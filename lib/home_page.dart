46a198d9c5cc8b7734659ae116697e71f3336efc
Chipes12 <carlos.andres_paez@outlook.com>
2022-09-11 20:11:21 -0500

    Tarea 2 lista

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,7 +9,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // TODO: completar todo lo necesario
+  var serviceController = TextEditingController();
+  int? currentRadio;
+  bool roundUp = false;
+  double tip = 0.0;
+  var radioGroup = {0: "Amazing 20%", 1: "Good 18%", 2: "Okay 15%"};
+  var percentages = [0.2, 0.18, 0.15];
 
   @override
   Widget build(BuildContext context) {
@@ -20,32 +26,130 @@ class _HomePageState extends State<HomePage> {
         children: [
           SizedBox(height: 14),
           ListTile(
-            leading: Icon(Icons.room_service),
+            leading: Icon(Icons.store),
             title: Padding(
               padding: EdgeInsets.only(right: 24),
-              child: TextField(),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                controller: serviceController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text("Cost of service"),
+                ),
+              ),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.dinner_dining),
+            leading: Icon(Icons.room_service),
             title: Text("How was the service?"),
           ),
-          Text("Aqui agregar el GRUPO de radio buttons"),
+          Container(
+            padding: EdgeInsets.only(left: 10, right: 10),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: radioGroupGenerator(),
+            ),
+          ),
           ListTile(
-            leading: Icon(Icons.credit_card),
+            leading: Icon(Icons.north_east),
             title: Text("Round up tip"),
+            trailing: Switch(
+              value: roundUp,
+              onChanged: (value) {
+                setState(() {
+                  roundUp = value;
+                });
+              },
+            ),
           ),
-          MaterialButton(
-            child: Text("CALCULATE"),
-            onPressed: null,
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: MaterialButton(
+              child: Text("CALCULATE"),
+              onPressed: () {
+                _tipCalculation();
+              },
+              color: Colors.green,
+            ),
           ),
-          Text("Tip amount: \$20.00"),
+          Text(
+            "Tip amount: \$${tip.toStringAsFixed(2)}",
+            textAlign: TextAlign.right,
+          ),
         ],
       ),
     );
   }
 
   void _tipCalculation() {
-    // TODO: completar
+    if (currentRadio == null) {
+      emptyRadio();
+    } else if (serviceController.text.isEmpty) {
+      emptyTextField();
+    } else {
+      tip = double.parse(serviceController.text) * percentages[currentRadio!];
+      if (roundUp) tip = tip.ceilToDouble();
+      setState(() {});
+    }
+  }
+
+  radioGroupGenerator() {
+    return radioGroup.entries
+        .map(
+          (radioElement) => ListTile(
+            leading: Radio(
+              value: radioElement.key,
+              groupValue: currentRadio,
+              onChanged: (int? selected) {
+                currentRadio = selected;
+                setState(() {});
+              },
+            ),
+            title: Text("${radioElement.value}"),
+          ),
+        )
+        .toList();
+  }
+
+  void emptyTextField() {
+    showDialog(
+      context: context,
+      builder: ((context) {
+        return AlertDialog(
+          title: Text("Error while calculating tip"),
+          content: Text("Service cost empty \nPlease fill the field"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Ok"),
+            )
+          ],
+        );
+      }),
+    );
+  }
+
+  void emptyRadio() {
+    showDialog(
+      context: context,
+      builder: ((context) {
+        return AlertDialog(
+          title: Text("Error while calculating tip"),
+          content:
+              Text("No tip percentage selected \nPlease select one option"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Ok"),
+            )
+          ],
+        );
+      }),
+    );
   }
 }
